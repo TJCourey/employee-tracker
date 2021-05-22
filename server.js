d7be98dbc186178895b84f7488f9ae88acb7c8d6
Thor138 <tjcourey84@gmail.com>
2021-05-22 15:28:13 -0600

    started adding code in to server.js, brought in console.table

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-
+const cTable = require("console.table");
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   database: "employee_db",
 });
 
-employeeQuestion = [
+const employeeQuestion = [
   {
     type: "input",
     name: "firstName",
@@ -31,7 +31,7 @@ employeeQuestion = [
     message: "(Optional) Manager ID#:",
   },
 ];
-roleQuestion = [
+const roleQuestion = [
   {
     type: "input",
     name: "title",
@@ -48,20 +48,149 @@ roleQuestion = [
     message: "Department ID#:",
   },
 ];
-departmentQuestion = [
+const departmentQuestion = [
   {
     type: "input",
     name: "department",
     message: "Department Name:",
   },
 ];
-
-const employeeView = () => {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM employee", (err, res) => {
+const navQuestion = [
+  {
+    type: "list",
+    name: "nav",
+    message: "Which area would you like access?",
+    choices: ["Employees", "Roles", "Department", "Quit Program"],
+  },
+];
+const empActionQuestion = [
+  {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: ["View", "Add", "Remove", "Edit Role", "Go Back"],
+  },
+];
+const actionQuestion = [
+  {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: ["View", "Add", "Remove", "Go Back"],
+  },
+];
+const viewTable = (loc) => {
+  //   console.log("Selecting all employees...\n");
+  connection.query(`SELECT * FROM ${loc}`, (err, res) => {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
   });
 };
+
+const addEmployee = () => {
+  inquirer.prompt(employeeQuestion).then((data) => {
+    console.log("Inserting a new employee...\n");
+    connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: data.firstName,
+        lastName: data.lastName,
+        role_id: data.roleId,
+        manager_id: data.managerId,
+      },
+      (err, res) => {
+        if (err) throw err;
+        console.log(`${res.affectedRows} Employee added\n`);
+      }
+    );
+  });
+};
+const addRole = () => {
+  inquirer.prompt(roleQuestion).then((data) => {
+    console.log("Inserting a new role...\n");
+    connection.query(
+      "INSERT INTO role SET ?",
+      {
+        title: data.title,
+        salary: data.salary,
+        department_id: data.departmentId,
+      },
+      (err, res) => {
+        if (err) throw err;
+        console.log(`${res.affectedRows} Role added\n`);
+      }
+    );
+  });
+};
+const addDepartment = () => {
+  inquirer.prompt(departmentQuestion).then((data) => {
+    console.log("Inserting a new department...\n");
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        name: data.department,
+      },
+      (err, res) => {
+        if (err) throw err;
+        console.log(`${res.affectedRows} Department added\n`);
+      }
+    );
+  });
+};
+const removeEmployee = (data) => {};
+
+initialQuestion = () => {
+  inquirer.prompt(navQuestion).then((data) => {
+    switch (data.nav) {
+      case "Employees": {
+        employeeActionQ();
+        break;
+      }
+      case "Roles": {
+        roleActionQ();
+        break;
+      }
+      case "Department": {
+        departmentActionQ();
+      }
+      case "Quit Program": {
+        console.log("Goodbye");
+        connection.end();
+      }
+    }
+  });
+};
+
+employeeActionQ = () => {
+  console.table(employeeView());
+  inquirer.prompt(empActionQuestion).then((data) => {
+    switch (data.nav) {
+      case "View": {
+        viewTable("employee");
+        break;
+      }
+      case "Add": {
+        addEmployee();
+        break;
+      }
+      case "Remove": {
+        removeEmployee();
+        break;
+      }
+      case "Edit Role": {
+        editRole();
+        break;
+      }
+      case "Go back": {
+        initialQuestion();
+      }
+    }
+  });
+};
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  viewTable("");
+});
