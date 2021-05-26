65bb23e973217f77c5558ce6ee6e0c016fe6f759
Thor138 <tjcourey84@gmail.com>
2021-05-25 19:02:39 -0600

    added foriegn keys, unfortunately the dont like to a Mercedes

--- employee_db.sql
+++ employee_db.sql
@@ -9,19 +9,23 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NULL
+  manager_id INT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DEC(10,4) NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL,
 );
 
 INSERT INTO employee (first_name, last_name, role_id)
